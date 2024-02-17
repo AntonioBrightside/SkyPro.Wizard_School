38114f57ee03f7b2294cf14585a8c8b324f79f27
AntonioBrightside <JilooPn/1013?>
2024-02-17 15:07:32 +0300

    Initial commit for wizard school

---
+++ src/main/java/com/amam/wizardschool/WizardSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.amam.wizardschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amam/wizardschool/WizardSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amam.wizardschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
